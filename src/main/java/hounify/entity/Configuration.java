943b4ea3d5f4c034f36a3b1ddb4d12977fc99a5b
gongmingbo <438793644@qq.com>
2018-11-07 14:19:05 +0800

    11

--- src/main/java/hounify/entity/Configuration.java
+++ src/main/java/hounify/entity/Configuration.java
@@ -12,7 +12,7 @@ import java.sql.Timestamp;
 @Table(name = "hg_configuration")
 public class Configuration {
 	/**
-	 * 主键id自增长
+	  * 主键id自增长
 	 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
